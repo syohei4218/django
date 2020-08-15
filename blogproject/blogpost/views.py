a0a56fc9621ba66cea73f245e5fcbe83a2f336fa
syohei4218 <syohei4218@gmail.com>
2020-08-15 09:40:05 +0000

    4章_完了

--- blogproject/blogpost/views.py
+++ blogproject/blogpost/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 from .models import BlogModel
 
 
@@ -10,3 +11,20 @@ class BlogList(ListView):
 class BlogDetail(DetailView):
     template_name = 'detail.html'
     model = BlogModel
+
+class BlogCreate(CreateView):
+    template_name = 'create.html'
+    model = BlogModel
+    fields = ('title', 'content', 'category')
+    success_url = reverse_lazy('list')
+
+class BlogDelete(DeleteView):
+    template_name = 'delete.html'
+    model = BlogModel
+    success_url = reverse_lazy('list')
+
+class BlogUpdate(UpdateView):
+    template_name = 'update.html'
+    model = BlogModel
+    fields = ('title', 'content', 'category')
+    success_url = reverse_lazy('list')
