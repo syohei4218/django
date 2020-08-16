229dafe1ba88ed4eb7f002b9d06161c4f64b9064
syohei4218 <syohei4218@gmail.com>
2020-08-16 04:58:19 +0000

    5-8

--- reviewproject/reviewpost/views.py
+++ reviewproject/reviewpost/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
+from .models import ReviewModel
 
 def signupview(request):
     if request.method == 'POST':
@@ -11,5 +13,21 @@ def signupview(request):
         except IntegrityError:
             return render(request, 'signup.html', {'error':'このユーザは既に登録されています。'})
     else:
-        return render(request, 'signup.html', {})
+        return redirect('login')
     return render(request, 'signup.html', {})
+
+def loginview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        user = authenticate(request, user_name=username_data, password=password_data)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    return render(request, 'login.html', {})
+
+def listview(request):
+    object_list = ReviewModel.objects.all()
+    return render(request, 'list.html', {'object_list':object_list})
